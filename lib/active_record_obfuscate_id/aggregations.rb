279ec8cc8dbfff5f5dc512d287527003b99207f9
alpaca-tc <alpaca-tc@alpaca.tc>
2018-03-23 23:21:47 +0900

    Fixes spec

--- lib/active_record_obfuscate_id/aggregations.rb
+++ lib/active_record_obfuscate_id/aggregations.rb
@@ -2,20 +2,24 @@
 
 module ActiveRecordObfuscateId
   module Aggregations
-    class ObfuscateId < BasicObject
+    class ObfuscateId
       def self.from_obfuscated_id(value)
-        new(value)
+        new ::ActiveRecordObfuscateId.coder.decode(value)
       end
 
-      delegate(:==, :===, to: :to_obfuscated_id)
+      delegate(:inspect, :to_s, :==, :===, to: :to_obfuscated_id)
       delegate_missing_to(:to_obfuscated_id)
+
+      attr_reader :raw_value, :integer_value, :obfuscated_id
 
       def initialize(raw_value)
         @raw_value = raw_value
+        @integer_value = ActiveRecord::Type.lookup(:integer).cast(raw_value)
+        @obfuscated_id = ::ActiveRecordObfuscateId.coder.encode(integer_value)
       end
 
       def to_obfuscated_id
-        @raw_value.to_obfuscated_id
+        obfuscated_id
       end
     end
 
@@ -23,8 +27,10 @@ module ActiveRecordObfuscateId
       composed_of(
         :"obfuscated_#{column_name}",
         class_name: 'ActiveRecordObfuscateId::Aggregations::ObfuscateId',
-        mapping: [%W[#{column_name} to_obfuscated_id]],
-        converter: ->(value) { ActiveRecordObfuscateId::Aggregations::ObfuscateId.from_obfuscated_id(value) }
+        mapping: [%W[#{column_name} raw_value]],
+        converter: ->(value) {
+          ActiveRecordObfuscateId::Aggregations::ObfuscateId.from_obfuscated_id(value)
+        }
       )
     end
   end
--- spec/active_record_obfuscate_id/aggregations_spec.rb
+++ spec/active_record_obfuscate_id/aggregations_spec.rb
@@ -10,28 +10,29 @@ RSpec.describe ActiveRecordObfuscateId::Aggregations do
   describe '.compose_of_obfuscated_id' do
     before do
       allow_any_instance_of(ActiveRecordObfuscateId::Coder).to receive(:encode).and_return(100)
+      allow_any_instance_of(ActiveRecordObfuscateId::Coder).to receive(:decode).and_return(1)
     end
 
     it 'assign value from aggregation' do
       expect(User.new(id: 1).id).to eq(1)
-      expect(User.new(obfuscated_id: 1).id).to eq(100)
+      expect(User.new(obfuscated_id: 100).id).to eq(1)
     end
 
     it 'calculates value with aggregation' do
       expect(User.new(id: 1).obfuscated_id).to eq(100)
-      expect(User.new(obfuscated_id: 1).id).to eq(100)
+      expect(User.new(obfuscated_id: 100).id).to eq(1)
     end
 
     it 'converts value from "1".to_obfuscated_id' do
       expect(User.where(id: 1).to_sql).to match(/"id" = 1/)
-      expect(User.where(obfuscated_id: 1).to_sql).to match(/"id" = 100/)
+      expect(User.where(obfuscated_id: 100).to_sql).to match(/"id" = 1/)
 
-      expect(Cart.where(obfuscated_user_id: 1).to_sql).to match(/"user_id" = 100/)
-      expect(Cart.joins(:user).merge(User.where(obfuscated_id: 1)).to_sql).to match(/"users"."id" = 100/)
+      expect(Cart.where(obfuscated_user_id: 100).to_sql).to match(/"user_id" = 1/)
+      expect(Cart.joins(:user).merge(User.where(obfuscated_id: 100)).to_sql).to match(/"users"."id" = 1/)
     end
 
     it 'defines #obfuscated_xxx' do
-      User.new(obfuscated_id: 1).obfuscated_id
+      expect(User.new(obfuscated_id: 1).obfuscated_id).to eq(100)
     end
   end
 end
